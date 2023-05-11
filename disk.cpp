6a92f9872f427a7eb0c0c4bd6b446aac0486c020
Ishaan Harry Kavoori <ishaanharry@live.com>
2023-05-11 14:09:57 -0700

    made disk.cpp

---
+++ disk.cpp
@@ -0,0 +1,14 @@
+/*
+ * DISK.CPP: program to generate a disk galaxy model composed of concentric rings
+ */
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define NUMRINGS = 16   // number of rings
+
+int main(int argc, char *argv){
+
+}
